f443dcc13aedc8a45a17a0a33320e14321f2377e
Игорь Галимский <galimskiy@awem.by>
2018-09-27 21:03:18 +0300

    Добавление пустого фрагмента today

--- app/src/main/java/galimski/igor/com/do_ing/MainActivity.java
+++ app/src/main/java/galimski/igor/com/do_ing/MainActivity.java
@@ -1,19 +1,63 @@
 package galimski.igor.com.do_ing;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView _bottomNavigationView;
+    FrameLayout _frameLayout;
+    FragmentManager _fragmentManager;
+
+    TodayFragment _todayFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _todayFragment = new TodayFragment();
+
+        _bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        _bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_today:
+
+                                UpdateFragment(_todayFragment);
+
+                                break;
+                            case R.id.action_schedules:
+
+                                //UpdateFragment(null);
+
+                                break;
+                            case R.id.action_feed:
+
+                                //UpdateFragment(null);
+
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    private void UpdateFragment(Fragment fragment)
+    {
+        _fragmentManager = this.getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = _fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLyout, fragment);
+        fragmentTransaction.commit();
     }
 }
